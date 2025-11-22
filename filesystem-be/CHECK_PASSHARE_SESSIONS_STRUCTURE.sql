aa7a157b472af9ce9119820bb48ceac8bb5537de
balaji-0505 <pabbathibalaji12@gmail.com>
2025-11-22 23:00:08 +0530

    Delete filesystem-be directory

--- filesystem-be/CHECK_PASSHARE_SESSIONS_STRUCTURE.sql
+++
@@ -1,7 +0,0 @@
--- Check the actual structure of the passhare_sessions table
-USE filesharing;
-
-DESCRIBE passhare_sessions;
--- or
-SHOW COLUMNS FROM passhare_sessions;
-
--- filesystem-be/src/main/java/com/file/system/springboot/model/ShareType.java
+++
@@ -1,9 +0,0 @@
-package com.file.system.springboot.model;
-
-public enum ShareType {
-    PRIVATE,
-    RESTRICTED,
-    PUBLIC
-}
-
-
