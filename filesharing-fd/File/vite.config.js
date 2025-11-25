fcdfdec068e5be46d61e488e46c32d69911d0963
balaji-0505 <pabbathibalaji12@gmail.com>
2025-11-25 23:42:50 +0530

    Update vite.config.js

--- filesharing-fd/File/vite.config.js
+++ filesharing-fd/File/vite.config.js
@@ -4,12 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api": {
-        target: "http://backend:8080",  // NO SLASH AT END
-        changeOrigin: true,
-        secure: false
-      }
-    }
+    proxy: {}  // REMOVE ALL PROXY
   }
 });
